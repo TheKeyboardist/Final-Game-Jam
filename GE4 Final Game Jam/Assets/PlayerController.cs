a199e2f143645fa086cc97d929c20c253921f123
Ivan Kravchenko <41616785+TheKeyboardist@users.noreply.github.com>
2021-04-22 21:53:20 -0400

    platforms and player placed

---
+++ GE4 Final Game Jam/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int stepLength;
+
+
+
+    void Start()
+    {
+        stepLength = 2;
+    }
+
+    void Update()
+    {
+        
+    }
+}
