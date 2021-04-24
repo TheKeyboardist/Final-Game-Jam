2fe045dc6e417498e78cc66d7a67060f217201e3
Ivan Kravchenko <41616785+TheKeyboardist@users.noreply.github.com>
2021-04-24 04:57:26 -0400

    start animation

--- GE4 Final Game Jam/Assets/Scripts/GameManager.cs
+++ GE4 Final Game Jam/Assets/Scripts/GameManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject moveCounter_GO;
+    
+    Text moveCounter_Text;
+
+    [SerializeField]
+    public int movesLeft;
+    void Awake()
     {
+        moveCounter_Text = moveCounter_GO.GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        movesLeft = 25;
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        moveCounter_Text.text = "Moves left: "  + movesLeft.ToString();
     }
+
 }
--- GE4 Final Game Jam/Assets/Scripts/PlayerController.cs
+++ GE4 Final Game Jam/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-   // PlayerInput input;
+    GameObject gameManager_GO_Ref;
+    GameManager gameManager_S_Ref;
+
+    public Animator animator;
+
     private PlayerActions playerActionsControls;
     public int stepLength;
     public Vector3 newCurrentPosition;
@@ -14,17 +18,16 @@ public class PlayerController : MonoBehaviour
     public bool animFinished;
 
 
-    //Materials
-    [SerializeField]
-    public Material pink;
 
 
     private void Awake()
     {
-        // input = new PlayerInput();
+        gameManager_GO_Ref = GameObject.FindGameObjectWithTag("GameManager");
+        gameManager_S_Ref = gameManager_GO_Ref.GetComponent<GameManager>();
         playerActionsControls = new PlayerActions();
     }
 
+    
 
     private void OnEnable()
     {
@@ -41,34 +44,46 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         stepLength = 2;
-        animFinished = true;
+        animFinished = false;
         newCurrentPosition = transform.position;
         playerActionsControls.CharacterControls.Movement.performed += ctx => Move(playerActionsControls.CharacterControls.Movement.ReadValue<Vector2>());
-
+        animator.SetTrigger("IntroTrigger");
+        StartCoroutine(DelayStart());
+        if(animFinished)
+        StopCoroutine(DelayStart());
     }
 
     void Update()
     {
-        Debug.Log(stepLength);
     }
 
     public void Move(Vector2 direction)
     {
-        float x = transform.position.x + playerActionsControls.CharacterControls.Movement.ReadValue<Vector2>().x * stepLength;
-        float y = transform.position.z + playerActionsControls.CharacterControls.Movement.ReadValue<Vector2>().y * stepLength;
-        nextPosition = new Vector3(x, transform.position.y, y);
-        Debug.Log("trying to move");
-        if(CanMove(direction))
+        if(animFinished && gameManager_S_Ref.movesLeft > 0)
         {
-            transform.position = nextPosition;
-            UpdateStats();
-        }  
+            float x = transform.position.x + playerActionsControls.CharacterControls.Movement.ReadValue<Vector2>().x * stepLength;
+            float y = transform.position.z + playerActionsControls.CharacterControls.Movement.ReadValue<Vector2>().y * stepLength;
+            nextPosition = new Vector3(x, transform.position.y, y);
+            Debug.Log("trying to move");
+            if (CanMove(direction))
+            {
+                transform.position = nextPosition;
+                UpdateStats();
+            }
+            gameManager_S_Ref.movesLeft--;
+        }
+       
     } 
 
     private bool CanMove(Vector2 direction)
     {
+        if (gameManager_S_Ref.movesLeft < 1)
+        {
+            return false;
+        }
         nextPosition = new Vector3(transform.position.x + direction.x*stepLength, transform.position.y, transform.position.z + direction.y*stepLength);
         RaycastHit hit;
+
         if (Physics.Raycast(nextPosition, Vector3.down, out hit) && hit.transform.parent.tag == "Tile")
         {
             return true;
@@ -96,9 +111,13 @@ public class PlayerController : MonoBehaviour
                 RedTile scriptRef = hit.transform.GetComponent<RedTile>();
                 scriptRef.ApplyEffect();
             }
+            if (Physics.Raycast(transform.position, Vector3.down, out hit) && hit.transform.tag == "FinishTag")
+            {
 
-            //changing frontend parameters
-            transform.Find("Top").GetComponent<Renderer>().material = hit.transform.GetComponent<Renderer>().material;
+            }
+
+                //changing frontend parameters
+                transform.Find("Top").GetComponent<Renderer>().material = hit.transform.GetComponent<Renderer>().material;
         }
 
     }
@@ -107,6 +126,15 @@ public class PlayerController : MonoBehaviour
     {
         stepLength = 2;
         transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+
+    IEnumerator DelayStart()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        animator.GetComponent<Animator>().enabled = false;
+        animFinished = true;
     }
 
 }
